6d48b0b4bab07199bab5eb2728c845e3c95094ad
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2023-02-16 18:39:07 -0300

    :sparkles: Feature: signup OK

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,12 +2,19 @@ import React from 'react';
 import '../Input/input.styles.css'
 
 
-export function Input({type, id, name, placeholder}) {
+export function Input({...others}) {
+    const {
+        type, 
+        id, 
+        name, 
+        placeholder, 
+        value,
+        onChange
+    } = others
 
     return (
         <>
-            <input type={type} id={id}  name={name} className='input' placeholder={placeholder}/>
-
+            <input type={type} id={id}  name={name} className='input' value={value} onChange={onChange} placeholder={placeholder} required/>
         </>
     )
 }
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,11 +1,23 @@
 import { AuthRoutes } from './auth.routes'
 import { LinkRoutes } from './link.routes'
+import { UserContext } from '../contexts/UserContext'
+import { useState } from 'react'
 
 export function Routes() {
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+
+    const context = {
+        email,
+        setEmail,
+        password,
+        setPassword
+    }
+
     return (
-        <>
+        <UserContext.Provider value={context}>
             <AuthRoutes />
             <LinkRoutes />
-        </>
+        </UserContext.Provider>
     )
 }
