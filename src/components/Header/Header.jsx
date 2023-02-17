f8d339a0c3d47c171217cf72b2b9fc97b6c3b401
Arthur Nepomuceno <arth.nepomuceno@gmail.com>
2023-02-16 22:56:43 -0300

    :construction: Build: functions for page links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,28 @@
-import styled from 'styled-components'
 import '../Header/header.styled.css'
+import styled from 'styled-components'
 import { useNavigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { UserContext } from '../../contexts/UserContext'
 
 export function Header() {
     const navigate = useNavigate()
-    function exit(){
+    const {
+        email,
+        setEmail,
+        setPassword,
+        setToken
+    } = useContext(UserContext)
+
+    function exit() {
+        setEmail('')
+        setPassword('')
+        setToken({})
         navigate('/')
     }
+
     return (
         <Div>
-            <p className='welcome'>Ola, !</p>
+            <p className='welcome'>Olá {email}!</p>
             <p className='exit' onClick={exit}>Sair</p>
         </Div>
     )
